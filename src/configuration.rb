c63bf8b3782ac6e9acdfb3f1ea11a4d5b236e2fb
Norman <nkoehring@nkoehring.de>
2010-07-21 02:59:13 +0200

    nice javascript canvas rotating background magic eyecandy!

--- src/configuration.rb
+++ src/configuration.rb
@@ -5,7 +5,7 @@ require 'redclothcoderay'
 
 
 configuration.preview_server_port = 3010
-configuration.preview_server_host = "localhost"
+configuration.preview_server_host = "0.0.0.0"
 
 configuration.sass_options = {
     :style => :compact
